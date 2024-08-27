63e0e597c0f65976b5ea26622aff38d78360985e
LuizAndrade <lcfsandrade@gmail.com>
2024-08-26 23:43:22 -0300

    Lógica criação plays

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -11,6 +11,28 @@ class PlaysController < ApplicationController
     @play = @match.plays.new(play_params)
     @play.match_id = @match.id
 
+   if params[:play][:games_won_dupla1].present? && params[:play][:games_won_dupla2].present?
+      games_won_dupla1 = params[:play][:games_won_dupla1].to_i
+      games_won_dupla2 = params[:play][:games_won_dupla2].to_i
+
+      # Atribuir games_won para os jogadores da dupla 1 e atualizar jogos e sets
+      @play.dupla1.each do |pp|
+        pp.games_won = games_won_dupla1
+        pp.player.games_won += games_won_dupla1 # Incrementar jogos ganhos
+        pp.player.games_lost += games_won_dupla2 # Incrementar jogos perdidos
+        pp.player.sets_won += 1 if games_won_dupla1 >= 6 # Incrementar sets ganhos
+        pp.player.save
+      end
+
+      # Atribuir games_won para os jogadores da dupla 2 e atualizar jogos e sets
+      @play.dupla2.each do |pp|
+        pp.games_won = games_won_dupla2
+        pp.player.games_won += games_won_dupla2 # Incrementar jogos ganhos
+        pp.player.games_lost += games_won_dupla1 # Incrementar jogos perdidos
+        pp.player.sets_won += 1 if games_won_dupla2 >= 6 # Incrementar sets ganhos
+        pp.player.save
+      end
+    end
 
     if @play.save!
       redirect_to match_path(@match), notice: 'Play criado com sucesso. 🟢'
@@ -39,7 +61,10 @@ class PlaysController < ApplicationController
   private
 
   def play_params
-    params.require(:play).permit(:play_number, play_players_attributes: [:player_id, :games_won, :id, :_destroy])
+    params.require(:play).permit(:play_number,
+                                :games_won_dupla1,
+                                :games_won_dupla2,
+                                play_players_attributes: [:player_id, :id, :_destroy])
   end
 
   def set_play
--- app/models/play.rb
+++ app/models/play.rb
@@ -4,4 +4,14 @@ class Play < ApplicationRecord
   has_many :players, through: :play_players
 
   accepts_nested_attributes_for :play_players, allow_destroy: true
+  
+  attr_accessor :games_won_dupla1, :games_won_dupla2
+
+  def dupla1
+    play_players.first(2)
+  end
+
+  def dupla2
+    play_players.last(2)
+  end
 end
