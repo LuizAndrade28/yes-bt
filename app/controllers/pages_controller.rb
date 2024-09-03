fefdcc954e1d9ea1b28b5f9d0eab560cdd24070f
LuizAndrade <lcfsandrade@gmail.com>
2024-09-03 15:44:00 -0300

    Pages controller and players order home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,17 @@
 class PagesController < ApplicationController
   def home
-    # Inicializando variáveis de jogadores
-    @players = Player.all
-    @female_players = Player.where(gender: 'Feminino')
+    # Inicializando variáveis de jogadores e Ordenando os jogadores
+    @players = Player.all.order(name: :asc)
+    @female_players = Player.where(gender: 'Feminino').order(games_won: :desc)
+    @players_ranking = @players.order(games_won: :desc)
 
-    # Ordenando os jogadores
-    @players = @players.order(games_won: :desc)
-    @female_players = @female_players.order(games_won: :desc)
-
-    # Outras variáveis e lógica de classificação
+    # Outras variáveis
     @matches = Match.all
-    @plays = Play.all
-    @play_players = PlayPlayer.all
-    @play = Play.new
+
+    # @female_players = @female_players.order(games_won: :desc)
+    # @plays = Play.all
+    # @play_players = PlayPlayer.all
+    # @play = Play.new
 
     # respond_to do |format|
     #   format.html
