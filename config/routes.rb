e86e6d8afb60f6a1bfcfe509557075e239a7f420
LuizAndrade <lcfsandrade@gmail.com>
2024-12-16 16:52:22 -0300

    Docker files create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
-  delete "delete_all_data" => "pages#delete_all_data", as: :delete_all_data
   get "export_data" => "pages#export_data", as: :export_data
+  delete "delete_all_data" => "pages#delete_all_data", as: :delete_all_data
 
   resources :players
   resources :matches do
